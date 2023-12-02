b9fe668fbcccd445e65d91797d3e0c68c45a5cb4
jinchiim <nellroll@syuin.ac.kr>
2023-12-02 15:44:10 +0900

    refactor(OutputView.java): 출력 format refactoring

--- src/main/java/menu/view/output/OutputView.java
+++ src/main/java/menu/view/output/OutputView.java
@@ -8,7 +8,7 @@ public class OutputView implements Printer {
 
     public void printMenuRecommendResult(List<String> menuTypes, List<RecommendDto> recommendDtos) {
         OutputFormatter outputFormatter = new OutputFormatter();
-        System.out.println("메뉴 추천 결과입니다.");
+        System.out.println("\n메뉴 추천 결과입니다.");
         System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
         System.out.println(outputFormatter.formatCategoryResult(menuTypes));
         for (RecommendDto recommendDto : recommendDtos) {
