3475c1e4314f155878ed7cc7598bfbd953323a45
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-12-02 16:06:44 +0900

    feat: 도메인 특수성 있는 상수값 정리

---
+++ src/main/java/menu/Constants/DomainConstants.java
@@ -0,0 +1,12 @@
+package menu.Constants;
+
+public class DomainConstants {
+    public static final int CATEGORY_NUMBER_START = 1;
+    public static final int CATEGORY_NUMBER_END = 5;
+    public static final int CATEGORY_MAX_RECOMMEND_AMOUNT = 2;
+    public static final int COACH_MIN_PEOPLE = 2;
+    public static final int COACH_MAX_PEOPLE = 5;
+    public static final int COACH_NAME_LENGTH_MIN = 1;
+    public static final int COACH_NAME_LENGTH_MAX = 20;
+    public static final int NON_AVAIL_FOOD_MAX = 2;
+}
