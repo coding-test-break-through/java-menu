e6db714a7eae8a059b678efc6e963723f012df59
jinchiim <nellroll@syuin.ac.kr>
2023-12-02 15:20:33 +0900

    feat(inputView.java):
    - 입력 view 구현

---
+++ src/main/java/menu/view/input/InputView.java
@@ -0,0 +1,25 @@
+package menu.view.input;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import menu.view.input.validator.CoachValidator;
+import menu.view.input.validator.HateFoodValidator;
+
+public class InputView implements Reader {
+
+    @Override
+    public String readLine() {
+        return Console.readLine();
+    }
+
+    public List<String> getCoach() {
+        System.out.println("점심 메뉴 추천을 시작합니다. \n");
+
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        return new CoachValidator().validate(readLine());
+    }
+
+    public List<String> getHateFoods() {
+        return new HateFoodValidator().validate(readLine());
+    }
+}
