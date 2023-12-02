057bd159e48220dafc2cb4c77ed3b24e2c792928
jinchiim <nellroll@syuin.ac.kr>
2023-12-02 15:21:00 +0900

    feat(InputError.java):
    - InputIllegalArgumentException 작성
    - Error 구문 enum 작성

---
+++ src/main/java/menu/view/input/error/InputError.java
@@ -0,0 +1,15 @@
+package menu.view.input.error;
+
+public enum InputError {
+
+    INPUT_IS_BLANK("입력한 값이 빈 칸 입니다."),
+    NOT_POSSIBLE_INPUT("입력할 수 없는 값입니다."),
+    COACH_COUNT_NOT_IN_RANGE("코치는 최소 2명 이상 입력해야 합니다."),
+    NOT_EXIST_MENU("존재하지 않는 메뉴입니다.");
+
+    final String message;
+
+    InputError(String message) {
+        this.message = message;
+    }
+}
