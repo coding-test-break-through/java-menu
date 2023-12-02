40dd55ef3de728af1c1f3ee0adf5a5f46462270f
jinchiim <nellroll@syuin.ac.kr>
2023-12-02 15:29:45 +0900

    feat(MenuController.java): MenuController 구현
    - 전체 흐름 로직 작성

---
+++ src/main/java/menu/controller/MenuController.java
@@ -0,0 +1,104 @@
+package menu.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import menu.domain.Recommender;
+import menu.domain.dto.RecommendDto;
+import menu.domain.menu.Menu;
+import menu.domain.menu.MenuType;
+import menu.util.RandomMenuRecommendGenerator;
+import menu.view.input.InputView;
+import menu.view.output.Output;
+import menu.view.output.OutputView;
+
+public class MenuController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public MenuController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        List<String> coaches = getCoaches();
+        List<Recommender> recommender = getCoachHateFood(coaches);
+        List<MenuType> menuTypes = menuRecommend(recommender);
+        showRecommendResult(menuTypes, recommender);
+    }
+
+    private List<String> getCoaches(){
+        List<String> coaches = receiveValidatedInput(inputView::getCoach);
+        return coaches;
+    }
+
+    private List<Recommender> getCoachHateFood(List<String> coaches){
+        List<Recommender> recommends = new ArrayList<>();
+
+        for (String name : coaches){
+            outputView.printfMessage(Output.INPUT_CAN_NOT_EAT_FOOD, name);
+            List<String> hateFoods = receiveValidatedInput(inputView::getHateFoods);
+            recommends.add(new Recommender(name, hateFoods));
+        }
+        return recommends;
+    }
+
+    private List<MenuType> menuRecommend(List<Recommender> recommenders){
+        List<MenuType> menuTypes = new ArrayList<>();
+        for (int index=0;index<5;index++){
+            MenuType menuType = pickMenuCategory(menuTypes);
+            menuTypes.add(menuType);
+            recommendToCoach(menuType, recommenders);
+        }
+        return menuTypes;
+    }
+
+    private void showRecommendResult(List<MenuType> menuTypes, List<Recommender> recommenders){
+        List<RecommendDto> recommenderResults = new ArrayList<>();
+
+        for (Recommender recommender : recommenders){
+            recommenderResults.add(recommender.getTotalRecommend());
+        }
+        List<String> menuTypeNames = MenuType.convertToNameList(menuTypes);
+
+        outputView.printMenuRecommendResult(menuTypeNames, recommenderResults);
+        outputView.printMessage(Output.RECOMMEND_RESULT_SUCCESS);
+    }
+
+    private void recommendToCoach(MenuType menuType, List<Recommender> recommenders){
+        for (Recommender recommender : recommenders){
+            List<String> menus = Menu.getMenuNamesByType(menuType);
+            recommender.recommendFood(menus);
+        }
+    }
+
+    private MenuType pickMenuCategory(List<MenuType> menuTypes){
+        MenuType menuType = MenuType.getMenuType(new RandomMenuRecommendGenerator().pickRandomNumber());
+        if (hasMinMenuType(menuTypes, menuType)){
+            return menuType;
+        }
+        return pickMenuCategory(menuTypes);
+    }
+
+    private static <T> boolean hasMinMenuType(List<T> list, T targetValue) {
+        Map<T, Long> counts = list.stream()
+                .filter(value -> value.equals(targetValue))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        return counts.getOrDefault(targetValue, 0L) < 2;
+    }
+
+    private <T> T receiveValidatedInput(Supplier<T> inputSupplier) {
+        try {
+            return inputSupplier.get();
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e);
+            return receiveValidatedInput(inputSupplier);
+        }
+    }
+}
