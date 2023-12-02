745c4dfab6f143960475eaa57f253f8b78ef6c84
jinchiim <nellroll@syuin.ac.kr>
2023-12-02 15:40:49 +0900

    refactor(OutputView.java): 사용하지 않는 부분 삭제

--- src/main/java/menu/view/output/OutputView.java
+++ src/main/java/menu/view/output/OutputView.java
@@ -2,17 +2,16 @@ package menu.view.output;
 
 import java.util.List;
 import menu.domain.dto.RecommendDto;
-import menu.domain.menu.MenuType;
 import menu.view.output.formatter.OutputFormatter;
 
 public class OutputView implements Printer {
 
-    public void printMenuRecommendResult(List<String> menuTypes, List<RecommendDto> recommendDtos){
+    public void printMenuRecommendResult(List<String> menuTypes, List<RecommendDto> recommendDtos) {
         OutputFormatter outputFormatter = new OutputFormatter();
         System.out.println("메뉴 추천 결과입니다.");
         System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
         System.out.println(outputFormatter.formatCategoryResult(menuTypes));
-        for (RecommendDto recommendDto : recommendDtos){
+        for (RecommendDto recommendDto : recommendDtos) {
             String result = outputFormatter.formatCoachResult(recommendDto);
             System.out.println(result);
         }
@@ -24,11 +23,6 @@ public class OutputView implements Printer {
 
     public void printError(IllegalArgumentException e) {
         System.out.println(e.getMessage());
-    }
-
-    @Override
-    public void println() {
-        System.out.println();
     }
 
     @Override
--- src/main/java/menu/view/output/Printer.java
+++ src/main/java/menu/view/output/Printer.java
@@ -2,8 +2,6 @@ package menu.view.output;
 
 public interface Printer {
 
-    void println();
-
     void printMessage(Output output);
 
     void printfMessage(Output output, Object... args);
